d7560e62d7e19c0647e8772a62527a345cad0151
hms1499 <thanvanhuy159@gmail.com>
2026-03-12 20:49:32 +0700

    Deploy contracts to mainnet with new wallet address
    - Contract addresses updated:
      - dca-vault: SP2CMK69QNY60HBG8BJ4X5TD7XX2ZT4XB62V13SV.dca-vault (TX: 857c51a15f5965288787f4e3adc14a5a767fa5f557dd8d4d1e1f8a9c48b0929a)
      - bitflow-sbtc-swap-router: SP2CMK69QNY60HBG8BJ4X5TD7XX2ZT4XB62V13SV.bitflow-sbtc-swap-router (TX: 4afc1a5c3842b10b83c4e0a70db3ae9d9b48e194de86e7e6205ab487e105b2d2)

--- src/lib/dca.ts
+++ src/lib/dca.ts
@@ -11,10 +11,10 @@ import {
 import { openContractCall } from "@stacks/connect";
 
 export const DCA_CONTRACT_ADDRESS =
-  "SP18GQ5APPBQ0QF1ZR2CTCW6AV63EKT6T4EFYJMNW";
+  "SP2CMK69QNY60HBG8BJ4X5TD7XX2ZT4XB62V13SV";
 export const DCA_CONTRACT_NAME = "dca-vault";
 export const DEFAULT_SWAP_ROUTER =
-  "SP18GQ5APPBQ0QF1ZR2CTCW6AV63EKT6T4EFYJMNW.bitflow-sbtc-swap-router";
+  "SP2CMK69QNY60HBG8BJ4X5TD7XX2ZT4XB62V13SV.bitflow-sbtc-swap-router";
 
 export const TARGET_TOKENS = [
   {
